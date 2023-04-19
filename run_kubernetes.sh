32645bddc344ecd8cc2cd99c70ce8431b8e898b1
Atul Mehta <atul.ze@gmail.com>
2023-04-19 19:16:57 +0530

    DockerImgeChanges

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=mehtaat/app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run apppod --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods -A
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward apppod 8000:80
 
